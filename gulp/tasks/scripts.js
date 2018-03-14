fa4f3179f190205df93ba82267f7aaf13471c9c9
Lemuel <reyeslemuel@hotmail.com>
2018-03-14 05:42:35 -0700

    add logo. create mixins and variables

--- gulp/tasks/scripts.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-uglify = require('gulp-uglify');
-
-gulp.task('scripts', function() {
-  return gulp.src('./app/assets/scripts/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./app/temp/scripts'));
-});
